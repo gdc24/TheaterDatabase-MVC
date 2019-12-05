bbe4cbd78641a2ef781bacdb5b9a768e4b1e62b5
gdc24 <gdc24@case.edu>
2019-12-05 17:16:56 -0500

    Add project files.

---
+++ TheaterDatabase/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TheaterDatabase
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/global-scripts").Include(
+                   "~/Plugins/jquery-3.4.1.js",
+                   "~/Plugins/popper.js",
+                   "~/Plugins/bootstrap.js",
+                   "~/Scripts/global.js"
+                   ));
+
+            bundles.Add(new StyleBundle("~/bundles/global-styles").Include(
+                "~/Styles/global.css"
+                ));
+        }
+    }
+}
