13d33b09645322045c4f6ea1704049cc2c8794c5
Ryan Arvizu <raa116@case.edu>
2019-12-09 15:23:30 -0500

    Update Cast.cs

--- TheaterDatabase/Models/Cast.cs
+++ TheaterDatabase/Models/Cast.cs
@@ -7,5 +7,27 @@ namespace TheaterDatabase.Models
 {
     public class Cast
     {
+        [Key]
+        [Display(Name = "Cast ID")]
+        
+        public int IntCastID {  get; set;  }
+        
+        public string StrVoicePart {  get; set;  }
+        
+        public string StrRole {  get; set;  }
+        
+        public Cast(int intCastID, string strVoicePart, string strRole)
+        {
+            this.IntCastID = intCastID;
+            this.StrVoicePart = strVoicePart;
+            this.StrRole = strRole;
+        }
+        
+        public Cast { }
+        
+        public static Cast Of(int intCastID, string strVoicePart, string strRole)
+        {
+            return new Cast(intCastID, strVoicePart, strRole);
+        }
     }
 }
