f0eb980896ed9c26c420e68d0d8af313e9282665
Ryan Arvizu <raa116@case.edu>
2019-12-09 15:12:19 -0500

    Update Exec.cs

--- TheaterDatabase/Models/Exec.cs
+++ TheaterDatabase/Models/Exec.cs
@@ -7,5 +7,24 @@ namespace TheaterDatabase.Models
 {
     public class Exec
     {
+        [Key]
+        [Display(Name = "Exec ID")]
+        
+        public int IntExecID {  get; set;  }
+        
+        public string StrPosition {  get; set;  }
+        
+        public Exec(int intExecID, string strPosition)
+        {
+            this.IntExecID = intExecID;
+            this.StrPosition = strPosition;
+        }
+        
+        public Exec() { }
+        
+        public static Exec Of(int intExecID, string strPosition)
+        {
+            return new Exec(intExecID, strPosition);
+        }
     }
 }
