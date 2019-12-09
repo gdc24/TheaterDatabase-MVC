76cda143ffbdf2d7fc2d55d3be809894dbe3f922
Ryan Arvizu <raa116@case.edu>
2019-12-09 15:29:22 -0500

    Update Staff.cs

--- TheaterDatabase/Models/Staff.cs
+++ TheaterDatabase/Models/Staff.cs
@@ -7,5 +7,24 @@ namespace TheaterDatabase.Models
 {
     public class Staff
     {
+        [Key]
+        [Display(Name = "Staff ID")]
+        
+        public int IntStaffID {  get; set;  }
+        
+        public string StrPosition {  get; set;  }
+        
+        public Staff(int intStaffID, string strPosition)
+        {
+            this.IntStaffID = intStaffID;
+            this.StrPosition = strPosition;
+        }
+        
+        public Staff() { }
+        
+        public static Staff Of(int intStaffID, string strPosition)
+        {
+            return new Staff(intStaffID, strPosition);
+        }
     }
 }
