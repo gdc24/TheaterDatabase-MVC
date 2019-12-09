37c5dd190269b011a41295b83e774ddc26f6d37b
Ryan Arvizu <raa116@case.edu>
2019-12-09 16:16:32 -0500

    added Display Names

--- TheaterDatabase/Models/Club.cs
+++ TheaterDatabase/Models/Club.cs
@@ -7,11 +7,27 @@ namespace TheaterDatabase.Models
 {
     public class Club
     {
+        [Key]
+        [Display(Name = "Club ID")]
+        
         public int IntClubID { get; set; }
+        
+        [Display(Name = "Club Name")]
+        
         public string StrClubName { get; set; }
+        
+        [Display(Name = "Advisor")]
+        
         public string StrAdvisor { get; set; }
+        
+        [Display(Name = "Umbrella Org")]
+        
         public string StrUmbrellaOrg { get; set; }
+        
+        [Display(Name = "Umbrella Org")]
+        
         public UmbrellaOrg UmbrellaOrg { get; set; }
+        
         private Club(int intClubID, string strClubName, string strAdvisor, string strUmbrellaOrg, UmbrellaOrg umbrellaOrg)
         {
             this.IntClubID = intClubID;
