286fcd495235fc00d709492aa3c63e855b07784c
Ryan Arvizu <raa116@case.edu>
2019-12-09 15:16:28 -0500

    Update Pit.cs

--- TheaterDatabase/Models/Pit.cs
+++ TheaterDatabase/Models/Pit.cs
@@ -7,5 +7,20 @@ namespace TheaterDatabase.Models
 {
     public class Pit
     {
+        [Key]
+        [Display(Name = "Pit ID")]
+        
+        public int IntPitID {  get; set;  }
+        
+        public string StrInstrument {  get; set;  }
+        
+        public int IntSeat {  get; set;  }
+        
+        public Pit() {  }
+        
+        public static Pit Of(int intPitID, string strInstrument, int intSeat)
+        {
+            return new Pit(intPitID, strInstrument, intSeat);
+        }
     }
 }
