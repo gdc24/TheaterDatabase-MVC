6069219a2d8c3057707420060e77b695176473ee
Ryan Arvizu <raa116@case.edu>
2019-12-09 15:42:08 -0500

    added foreign key

--- TheaterDatabase/Models/Exec.cs
+++ TheaterDatabase/Models/Exec.cs
@@ -14,17 +14,23 @@ namespace TheaterDatabase.Models
         
         public string StrPosition {  get; set;  }
         
-        public Exec(int intExecID, string strPosition)
+        public Member Member {  get; set;  }
+        
+        public int IntMemberID {  get; set;  }
+        
+        public Exec(int intExecID, string strPosition, int intMemberID, Member member)
         {
             this.IntExecID = intExecID;
             this.StrPosition = strPosition;
+            this.IntMemberID = intMemberID;
+            this.member = member;
         }
         
         public Exec() { }
         
-        public static Exec Of(int intExecID, string strPosition)
+        public static Exec Of(int intExecID, string strPosition, int intMemberID, Member member)
         {
-            return new Exec(intExecID, strPosition);
+            return new Exec(intExecID, strPosition, intMemberID, member);
         }
     }
 }
