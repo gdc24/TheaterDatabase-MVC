d7811c85a32d35c975a31056bb5dd1494b028fbb
Ryan Arvizu <raa116@case.edu>
2019-12-09 16:22:54 -0500

    added Display Names

--- TheaterDatabase/Models/Pit.cs
+++ TheaterDatabase/Models/Pit.cs
@@ -13,15 +13,27 @@ namespace TheaterDatabase.Models
         
         public int IntPitID {  get; set;  }
         
+        [Display(Name = "Instrument")]
+        
         public string StrInstrument {  get; set;  }
         
+        [Display(Name = "Seat")]
+
         public int IntSeat {  get; set;  }
+        
+        [Display(Name = "Member")]
         
         public Member Member {  get; set;  }
         
+        [Display(Name = "Member ID")]
+        
         public int IntMemberID {  get; set;  }
         
+        [Display(Name = "Show")]
+        
         public Show Show {  get; set;  }
+        
+        [Display(Name = "Show ID")]
         
         public int IntShowID {  get; set;  }
         
