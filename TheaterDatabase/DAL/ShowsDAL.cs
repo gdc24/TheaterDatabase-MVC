5b94eb582bbc8df120763d05c477389e008ab62e
nac71 <43797817+nac71@users.noreply.github.com>
2019-12-09 14:29:58 -0500

    Update ShowsDAL.cs

--- TheaterDatabase/DAL/ShowsDAL.cs
+++ TheaterDatabase/DAL/ShowsDAL.cs
@@ -52,6 +52,98 @@ namespace TheaterDatabase.DAL
 
             return retval;
         }
+        
+        public static bool InsertShow(Show show)
+        {
+
+            // create and open a connection
+            NpgsqlConnection conn = DatabaseConnection.GetConnection();
+            conn.Open();
+
+            // Define a query
+            string query = "INSERT INTO shows" +
+                           " (\"strName\", \"strAuthor\", \"intBudget\", \"ysnIsMusical\", \"intClubID\", \"intDateID\")" +
+                           " VALUES" +
+                           " (@strName, @strAuthor, @intBudget, @ysnIsMusical, @intClubID, @intDateID);";
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("strName", show.StrName);
+            cmd.Parameters.AddWithValue("strAuthor", show.StrAuthor);
+            cmd.Parameters.AddWithValue("intBudget", show.IntBudget);
+            cmd.Parameters.AddWithValue("ysnIsMusical", show.YsnIsMusical);
+            cmd.Parameters.AddWithValue("intClubID", show.IntClubID);
+            cmd.Parameters.AddWithValue("intDateID", show.IntDateID);
+
+            // Execute a query
+            int result = cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            if (result == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool UpdateShow(Show show)
+        {
+
+            // create and open a connection
+            NpgsqlConnection conn = DatabaseConnection.GetConnection();
+            conn.Open();
+
+            // Define a query
+            string query = "UPDATE dates " +
+                           " SET \"strName\" = @strName" +
+                           " \"strAuthor\" = @strAuthor" +
+                           " \"intBudget\" = @intBudget" +
+                           " \"ysnIsMusical\" = @ysnIsMusical" +
+                           " \"intClubID\" = @intClubID" +
+                           " \"intDateID\" = @intDateID" +
+                           " WHERE \"id\" = @id;";
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("strName", show.StrName);
+            cmd.Parameters.AddWithValue("strAuthor", show.StrAuthor);
+            cmd.Parameters.AddWithValue("intBudget", show.IntBudget);
+            cmd.Parameters.AddWithValue("ysnIsMusical", show.YsnIsMusical);
+            cmd.Parameters.AddWithValue("intClubID", show.IntClubID);
+            cmd.Parameters.AddWithValue("intDateID", show.IntDateID);
+            cmd.Parameters.AddWithValue("id", show.Id);
+            
+            // Execute a query
+            int result = cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            if (result == 1)
+                return true;
+            else
+                return false;
+
+        }
+
+        public static bool DeleteShow(Show show)
+        {
+
+            // create and open a connection
+            NpgsqlConnection conn = DatabaseConnection.GetConnection();
+            conn.Open();
+
+            // Define a query
+            string query = "DELETE FROM shows WHERE \"id\" = @id ";
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("intDateID", show.id);
+
+            // Execute a query
+            int result = cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            if (result == 1)
+                return true;
+            else
+                return false;
+        }
+
 
     }
 }
