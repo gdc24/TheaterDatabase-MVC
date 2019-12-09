a8b3d0d65a552dc9a8e3addb97613e8187477cba
Ryan Arvizu <raa116@case.edu>
2019-12-09 15:54:04 -0500

    added Show foreign key

--- TheaterDatabase/Models/Pit.cs
+++ TheaterDatabase/Models/Pit.cs
@@ -20,20 +20,26 @@ namespace TheaterDatabase.Models
         
         public int IntMemberID {  get; set;  }
         
-        public Pit(int intPitID, string strInstrument, int intSeat, int intMemberID, Member member) 
+        public Show Show {  get; set;  }
+        
+        public int IntShowID {  get; set;  }
+        
+        public Pit(int intPitID, string strInstrument, int intSeat, int intMemberID, Member member, int intShowID, Show show) 
         {
             this.IntPitID = intPitID;
             this.StrInstrument = strInstrument;
             this.IntSeat = intSeat;
             this.IntMemberID = intMemberID;
             this.Member = member;
+            this.IntShowID = intShowID;
+            this.Show = show;
         }
         
         public Pit() {  }
         
-        public static Pit Of(int intPitID, string strInstrument, int intSeat, int intMemberID, Member member)
+        public static Pit Of(int intPitID, string strInstrument, int intSeat, int intMemberID, Member member, int intShowID, Show show)
         {
-            return new Pit(intPitID, strInstrument, intSeat, intMemberId, member);
+            return new Pit(intPitID, strInstrument, intSeat, intMemberId, member, intShowID, show);
         }
     }
 }
